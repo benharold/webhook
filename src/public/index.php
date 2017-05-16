60f0d70ee4b84bb4e87b23758b7f44b2a1594977
Ben Harold <benharold@mac.com>
2017-05-15 21:03:37 -0400

    require php 7.1

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+if (version_compare('7.1.0', PHP_VERSION, '>')) {
+    printf('This application requires PHP 7.1. You are using PHP %s',
+        PHP_VERSION);
+    exit;
+}
+
 require '../../vendor/autoload.php';
 
 require '../routes.php';
